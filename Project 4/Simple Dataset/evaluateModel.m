5901d38da3631af7f11394c99e0073d440e9ac0e
panospro <panagiotisskaki@yahoo.gr>
2024-03-09 18:24:41 +0200

    Update evaluateModel.m

--- Project 4/Simple Dataset/evaluateModel.m
+++ Project 4/Simple Dataset/evaluateModel.m
@@ -1,14 +1,14 @@
 function evaluateModel(tstData, valFis)
     Y = round(evalfis(tstData(:,1:end-1), valFis));
     Y = max(min(Y, 2), 1); % Y values be within [1, 2]
-        
+
     % Error Matrix
     errorMatrix = confusionmat(tstData(:,end), Y);
     figure();
     confusionchart(tstData(:,end),Y);
 
     [overallAccuracy, producersAccuracy, usersAccuracy, kHat] = calculateMetrics(errorMatrix);
-    
+
     % Display results
     fprintf('Overall Accuracy = %f \n', overallAccuracy);
     fprintf('Khat = %f \n', kHat);
@@ -16,13 +16,13 @@ function evaluateModel(tstData, valFis)
     fprintf('User Accuracy = %f \n', usersAccuracy);
 end
 
-% Define the function to calculate performance metrics
+% Function to calculate performance metrics
 function [overallAccuracy, producersAccuracy, usersAccuracy, kHat] = calculateMetrics(errorMatrix)
     N = sum(errorMatrix, 'all');
     overallAccuracy = sum(diag(errorMatrix)) / N;
-    
-    producersAccuracy = diag(errorMatrix) ./ sum(errorMatrix, 2)';
-    usersAccuracy = diag(errorMatrix) ./ sum(errorMatrix, 1);
+
+    producersAccuracy = diag(errorMatrix)' ./ sum(errorMatrix, 1)
+    usersAccuracy = diag(errorMatrix)' ./ sum(errorMatrix, 2)'
 
     p1 = sum(errorMatrix(1, :)) * sum(errorMatrix(:, 1)) / N ^ 2;
     p2 = sum(errorMatrix(2, :)) * sum(errorMatrix(:, 2)) / N ^ 2;
